297296e975dea37bb22cfb724305c2ffee745886
Samir Costa <samir.coutinho@fortaleza.ce.gov.br>
2017-01-26 14:28:04 -0300

    change http client library

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM node:alpine
 
-COPY index.js .
+WORKDIR /app
+
 COPY package.json .
 
 RUN set -xe && npm install --no-cache
+
+COPY index.js .
 
 ENTRYPOINT ["node"]
 CMD ["index.js"]
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const httpclient = require('unirest');
 
-const app = new Telegraf(process.env.BOT_TOKEN)
+const app = new Telegraf(process.env.BOT_TOKEN);
 
 app.command('start', (ctx) => {
-  console.log('start', ctx.from)
-  ctx.reply('Bem-vindo!')
-})
+	console.log('start', ctx.from);
+	ctx.reply('Bem-vindo!');
+});
 
-app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+app.hears('hi', (ctx) => ctx.reply('Hey there!'));
 
-app.on('sticker', (ctx) => ctx.reply('👍'))
+app.on('sticker', (ctx) => ctx.reply('👍'));
 
-app.startPolling()
+app.command('test', (ctx) => {
+	console.log('test', ctx.form);
+
+	httpclient.get('https://dns.google.com/resolve?name=google.com')
+		.headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+		// .send({ "parameter": 23, "foo": "bar" })
+		.end(function (response) {
+			var json = JSON.parse(response.body);
+			console.log(json.Status);
+			ctx.reply(json.Status);
+		});
+});
+
+app.startPolling();
